760f5c9d57f2d0dbf3e86ce6846ccb727b78cb4c
d3fineme <felipe-alves-09@hotmail.com>
2016-06-16 18:28:43 -0300

    Página Inicial e Painel
    Arrumado o problema da página inicial e do painel de atendimento onde
    quando diminuía o tamanho da tela bagunçava toda a tela.

--- Painel.php
+++ Painel.php
@@ -8,13 +8,13 @@ $conexao = mysql_connect($host, $user, $pass) or die (mysql_error());
 mysql_select_db($banco) or die (mysql_error());
 ?>
 
-<?php 
+<?php
 session_start();
 if(!isset($_SESSION["user"]) || !isset($_SESSION["password"])){
     header("Location: login.php");
     exit;
 }
-    $NomeUser=$_SESSION["user"];
+$NomeUser=$_SESSION["user"];
 
 ?>
 
@@ -26,16 +26,28 @@ if(!isset($_SESSION["user"]) || !isset($_SESSION["password"])){
     <link rel="shortcut icon" href="css/imagens/parae.ico" type="image/x-icon" />
     <link href="css/formulario.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="css/fonte"/>
+    <div class="logo2"><a title="<?php echo $title;?>"><img src="css/imagens/paraefinal.png" onclick="location.href='paginicial.php'" alt="<?php echo $title;?>" title="<?php echo $title;?>" width="200" height="78"/> </a> </div>
+    <div id="sair">Olá , <?php echo $NomeUser ?> | <a href="logout.php">Sair</a></div><br>
 </head>
 <body background="css/imagens/vaga.png.jpg">
-    <div id="pagini" class="form bradius">
-        <div class="message"></div>
-        <div class="logo2"><a title="<?php echo $title;?>"><img src="css/imagens/parae.png" onclick="location.href='paginicial.php'" alt="<?php echo $title;?>" title="<?php echo $title;?>" width="200" height="78"/> </a> </div>
-        <div id="sair">Olá , <?php echo $NomeUser ?> | <a href="logout.php">Sair</a></div><br>
-        <input type="submit" class="sb bradius2" value="Entrada Veículo" onclick="location.href='PainelE.php?passo='" />
-        <input type="submit" class="sb bradius2" value="Saída Veiculo" onclick="location.href='PainelS.php?passo='"/>
-        <input type="submit" class="sb bradius2" value="Cadastrar Cliente" onclick="location.href='FormularioCadastroCliente.php'"/>
-        <input type="submit" class="sb bradius2" value="Consultar Cliente" onclick="location.href='FormularioBuscaCliente.php'"/>
+<center></center><div class="row-fluid">
+    <div class="span3"></div>
+    <div class="span3">
+        <center><h2><em><strong>ATENDIMENTO:</strong></em></h2>
+
     </div>
+    <div class="span3">
+        <center><button class="sb bradius 2 btn-primary" onclick="location.href='IniciaAtendimento.php?ia='">Entrada Veículo</button>
+    </div><br>
+    <div class="span3">
+        <center><button class="sb bradius 2 btn-primary" onclick="location.href='PainelS.php?passo='">Saída Veículo</button>
+    </div><br>
+    <div class="span3">
+        <center><button class="sb bradius 2 btn-primary" onclick="location.href='FormularioCadastroCliente.php'">Cadastrar Cliente</button>
+    </div><br>
+    <div class="span3">
+        <center><button class="sb bradius 2 btn-primary" onclick="location.href='FormularioBuscaCliente.php'">Consultar Cliente</button>
+    </div><br>
+</div>
 </body>
 </html>
--- paginicial.php
+++ paginicial.php
@@ -8,15 +8,15 @@ $conexao = mysql_connect($host, $user, $pass) or die (mysql_error());
 mysql_select_db($banco) or die (mysql_error());
 ?>
 
-<?php 
+<?php
 session_start();
 if(!isset($_SESSION["user"]) || !isset($_SESSION["password"])){
     header("Location: login.php");
     exit;
 } else {
-  if ($_SESSION["tuser"]==1) {
-    header("Location: Painel.php?passo=");
-}
+    if ($_SESSION["tuser"]==1) {
+        header("Location: Painel.php?passo=");
+    }
     $NomeUser=$_SESSION["user"];
 
 }
@@ -33,21 +33,30 @@ if(!isset($_SESSION["user"]) || !isset($_SESSION["password"])){
     <link rel="stylesheet" type="text/css" href="css/fonte"/>
 </head>
 <body background="css/imagens/vaga.png.jpg">
-    <div id="pagini" class="form bradius">
-        <div class="message"></div>
-        <div class="logo2"><a title="<?php echo $title;?>"><img src="css/imagens/parae.png" alt="<?php echo $title;?>" title="<?php echo $title;?>" width="200" height="78"/> </a> </div>
-        <div id="sair">Olá , <?php echo $NomeUser ?> | <a href="logout.php">Sair</a></div>
-        <input type="submit" class="sb bradius2" value="Cadastrar Usuário" onclick="location.href='FormularioCadastroUsuario.php'" />
-        <input type="submit" class="sb bradius2" value="Cadastrar Cliente" onclick="location.href='FormularioCadastroCliente.php'"/>
-        <input type="submit" class="sb bradius2" value="Cadastrar Vaga" onclick="location.href='FormularioCadastroVaga.php'"/>
-        <input type="submit" class="sb bradius2" value="Cadastrar Convênio" onclick="location.href='FormularioCadastroConvenio.php'"/>
-    </div>
-    <input type="submit" class="sb bradius3" value="Consultar Usuário" onclick="location.href='FormularioBuscaUsuario.php'"/>
-    <input type="submit" class="sb bradius4" value="Consultar Cliente" onclick="location.href='FormularioBuscaCliente.php'"/>
-    <input type="submit" class="sb bradius4" value="Consultar Vaga" onclick="location.href='ConsultaVaga.php'"/>
-    <input type="submit" class="sb bradius4" value="Consultar Convênio" onclick="location.href='ConsultaConvenio.php'"/><br>
-    <input type="submit" class="sb bradius5" value="Atendimento" value="Cadastrar Convênio" onclick="location.href='Painel.php'"/>
-    <input type="submit" class="sb bradius5" value="Relatório" onclick="location.href='FormularioRelatorio.php'"/>
-
+<div id="pagini" class="form bradius">
+    <div class="message"></div>
+    <div class="logo2"><a title="<?php echo $title;?>"><img src="css/imagens/paraefinal.png" alt="<?php echo $title;?>" title="<?php echo $title;?>" width="200" height="78"/> </a> </div>
+    <div id="sair">Olá , <?php echo $NomeUser ?> | <a href="logout.php">Sair</a></div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <center><tr class="success">
+            <button class="btn-default" onclick="location.href='FormularioCadastroUsuario.php'"><td>Cadastrar Usuário</td></button>
+            <button class="btn-default" onclick="location.href='FormularioCadastroCliente.php'"><td>Cadastrar Cliente</td></button>
+            <button class="btn-default" onclick="location.href='FormularioCadastroVaga.php'"><td>Cadastrar Vaga</td></button>
+            <button class="btn-default" onclick="location.href='FormularioCadastroConvenio.php'"><td>Cadastrar Convênio</td></button>
+        </tr></center>
+    <center><tr class="success">
+            <button class="btn-default" onclick="location.href='ConsultaUsuario.php'"><td>Consultar Usuário</td></button>
+            <button class="btn-default" onclick="location.href='ConsultaCliente.php'"><td>Consultar Cliente</td></button>
+            <button class="btn-default" onclick="location.href='ConsultaVaga.php'"><td>Consultar Vaga</td></button>
+            <button class="btn-default" onclick="location.href='ConsultaConvenio.php'"><td>Consultar Convênio</td></button>
+        </tr></center>
+    <center><tr class="success">
+            <button class="btn-default" onclick="location.href='Painel.php'"><td>Atendimento</td></button>
+            <button class="btn-default" onclick="location.href='Relatorio.php'"><td>Relatórios</td></button>
+        </tr></center>
+</div>
 </body>
-</html>
